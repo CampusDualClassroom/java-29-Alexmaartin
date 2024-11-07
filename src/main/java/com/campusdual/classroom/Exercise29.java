527196a65b612392e8549cf360058504de2054ff
Alejandro Martín Millarengo <alexmarmill@gmail.com>
2024-11-07 17:18:31 +0100

    Exercise 29

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,11 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
-
+        try {
+            int div = 0/0;
+            System.out.println("PROBANDO.");
+        }catch (Exception e){
+            System.out.println("Evitar propagación");
+        }
     }
 }
